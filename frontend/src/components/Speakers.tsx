31a488b1a8d976bebfc25c277036ec61b490d072
Gbadamosi Bashir Olamide <gbadamosibashir1738@gmail.com>
2026-05-15 13:14:44 +0100

    fix: adjust text size for speaker names in Speakers component

--- frontend/src/components/Speakers.tsx
+++ frontend/src/components/Speakers.tsx
@@ -97,7 +97,7 @@ export default function Speakers() {
                                 </div>
                                 
                                 <div className="p-6 bg-background relative z-20 grow flex flex-col">
-                                    <p className="text-xl  font-bold text-white mb-3 transition-colors">{speaker.name}</p>
+                                    <p className="text-lg  font-bold text-white mb-3 transition-colors">{speaker.name}</p>
                                     <div className="flex flex-col gap-1 mt-auto">
                                         <span className="text-primary font-medium text-xs sm:text-sm tracking-wide uppercase">{speaker.role}</span>
                                         <span className="text-gray-400 font-light text-sm sm:text-base">{speaker.company}</span>
